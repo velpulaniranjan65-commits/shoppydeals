347b41f187e2c8a5f9f27a373b3e5b6b94ba9283
Keerthana-Ramisetty <keerthanaramisetty972@gmail.com>
2026-06-01 14:23:09 +0100

    fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
+
 import type { NextConfig } from "next";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const apiUrl =
+  process.env.NEXT_PUBLIC_API_URL ||
+  "http://localhost:5000";
 
 const nextConfig: NextConfig = {
+  turbopack: {},
+
   images: {
     remotePatterns: [
       {
